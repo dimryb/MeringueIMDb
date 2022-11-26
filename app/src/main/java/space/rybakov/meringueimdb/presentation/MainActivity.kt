262b7e2ce5798087629606a39c880817994e9e57
dmitry <dimryb@bk.ru>
2022-11-26 16:38:55 +0700

    Разделить на слои

--- app/src/main/java/space/rybakov/meringueimdb/presentation/MainActivity.kt
+++ app/src/main/java/space/rybakov/meringueimdb/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package space.rybakov.meringueimdb
+package space.rybakov.meringueimdb.presentation
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import space.rybakov.meringueimdb.R
 import space.rybakov.meringueimdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
